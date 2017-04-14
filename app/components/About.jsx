63189b1e9dee35f4b5c819106f83e3e4487c3ce9
Andrew Day <Andrew Day>
2017-04-14 13:37:07 +1000

    Update About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,11 @@ var React = require('react');
 
 var About = (props) => {
   return (
-        <h3>About component</h3>
-      );
+    <div>
+        <h3>About</h3>
+        <p>This is the about page.</p>
+    </div>
+    );
 }
 
 module.exports = About;
